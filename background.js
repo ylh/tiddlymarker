ylh/tiddlymarker

commit 87a4bd5985af489e704efb2bdd9810ce73d89ea0

put tab-over mode out of its misery
this commit started its life with much more green.

having spent an entire day steadily implementing the blasted thing, it turns out
that the sort of access content scripts have to page scopes is just not enough.
perhaps some day in the future we can reconsider this after writing a TW plugin
that can pick up on postMessage calls or something, but as it stands, there just
isn't enough power in executeScript(). webserver mode is so good that the only
part of this that honestly hurts is the wasted time.

don't write browser code, people

background.js
@@ -38,15 +38,19 @@ const catch_bookmark = async () => {
 		browser.storage.local.set({state: 'done'});
 	}
 }
+/* if i were getting weird edge cases i would simply escape everything */
+const arg = str => `"${str.split("").map(single =>
+	`\\u${`0000${single.charCodeAt(0).toString(16).toUpperCase()}`.slice(-4)}`
+).join("")}"`;
+
+const literal_of = o => o === undefined
+	? "undefined"
+	: `JSON.parse(${arg(JSON.stringify(o))})`;
 
 const do_bookmark = async () => {
 	const prefs = await browser.storage.sync.get(defaults.sync),
 	      bookmark = await browser.storage.local.get(Object.keys(tab_reads)),
 	      {rawtitle, title, url, icon} = bookmark;
-	/* if i were getting weird edge cases i would simply escape everything */
-	const arg = str => `"${str.split("").map(c =>
-		`\\u${`0000${c.charCodeAt(0).toString(16).toUpperCase()}`.slice(-4)}`
-	).join("")}"`;
 	const creation_error = (error, field) =>
 		error.message.startsWith("Missing host permission") ? {
 			errortitle: msg("permissionErrorTitle"),
@@ -77,14 +81,11 @@ const do_bookmark = async () => {
 		}
 	}
 	try {
-		let iconarg = icon === undefined
-			? "undefined"
-			: `JSON.parse(${arg(JSON.stringify(favicon_fmt_out))})`;
-
 		bookmark_fmt_out = (await browser.tabs.executeScript({
 			code: `((rawtitle, title, url, icon) => {
 				${prefs.bookmark_fmt}
-			})(${arg(rawtitle)}, ${arg(title)}, ${arg(url)}, ${iconarg})`
+			})(${arg(rawtitle)}, ${arg(title)}, ${arg(url)},
+			   ${literal_of(icon)})`
 		}))[0];
 	} catch (e) {
 		return creation_error(e, "bookmark_fmt");
@@ -327,10 +328,7 @@ const sends = {
 	)).catch(e => e.hasOwnProperty('errortitle') ? e : {
 		errortitle: msg("unknownErrorTitle"),
 		details: e.toString()
-	})/*,
-	tabover: (prefs, bookmark, favicon) => new Promise((resolve, reject) => {
-
-	})*/
+	})
 };
 
 /* this seemed like it was going to be bigger and justify its scaffolding more
@@ -344,7 +342,6 @@ const handler_tree = {
 	local: {
 		state: (changes, change) => {
 			let nv = storage_cache.state = change.newValue;
-			console.log(`nv: ${nv}`);
 			badge({
 				ready: {text: "", fg: null, bg: null},
 				unfinished: {text: "!", fg: "white", bg: "#F80B"},

options.html
@@ -44,7 +44,6 @@
 					<span>
 						<select disabled autocomplete="off" id="savingmode">
 							<option value="download" data-msg="savingmode_downloadLabel"></option>
-							<!--<option value="tabover">Tab-Over</option>!-->
 							<option value="webserver" data-msg="savingmode_webserverLabel"></option>
 						</select>
 					</span>
@@ -57,10 +56,6 @@
 							<dt data-msg="savingmode_downloadLabel"></dt>
 							<dd data-msg="savingmode_downloadDesc"></dd>
 						</div>
-						<!--<div>
-							<dt>Tab-Over</dt>
-							<dd>If the specified destination TW is open, tab over to it; otherwise, open it. Import the appropriate tiddlers. If it was already open, tab back; if it was opened as a result of bookmarking, close it.</dd>
-						</div>!-->
 						<div>
 							<dt data-msg="savingmode_webserverLabel"></dt>
 							<dd data-msg="savingmode_webserverDesc"></dd>
@@ -68,29 +63,6 @@
 					</dl>
 				</div>
 			</section>
-			<!--<section id="tabover_prefs" class="greyout">
-				<h2>Tab-Over Mode</h2>
-				<div>
-					<span>
-						<input disabled type="text" autocomplete="off" id="taburi">
-					</span>
-					<label for="taburi" class="greyout">Search Prefix</label>
-				</div>
-				<div>
-					<span></span>
-					<small class="taburi greyout">This mode acts on the first tab found whose URL begins with this. In the event that no such tab is found, it opens a tab with the prefix as the full URL.</small>
-				</div>
-				<div>
-					<span>
-						<input disabled type="checkbox" autocomplete="off" id="closedone">
-					</span>
-					<label for="closedone" class="greyout">Close When Done</label>
-				</div>
-				<div>
-					<span></span>
-					<small class="closedone greyout">When no open tab is found, close it after attempting to save.</small>
-				</div>
-			</section>!-->
 			<section id="webserver_prefs" class="greyout">
 				<h2 data-msg="savingmode_webserverHeading"></h2>
 				<div>

options.js
@@ -109,15 +109,9 @@ const controls = {
 		pop: pref,
 		activate: rel({
 			download: [],
-			/*tabover: ["tabover_prefs"],*/
 			webserver: ["webserver_prefs"]
 		})
 	},
-	/*tabover_prefs: {
-		root: false,
-		activate: always(["taburi"])
-	},
-	taburi: ordinary_child,*/
 	webserver_prefs: {
 		root: false,
 		activate: always(["address", "auth", "safety"])

storage.js
@@ -6,7 +6,6 @@ const defaults = {
 		careful: false,
 		reqfav: true,
 		savingmode: 'download',
-		/*taburi: "",*/
 		address: "",
 		auth: false,
 		username: "",

tabs.js
@@ -35,12 +35,10 @@ const favicon_req = tab => new Promise((resolve, reject) => {
 		let mime = this.getResponseHeader('Content-Type');
 		let [type, subtype] = mime.split("/");
 
-		if (type !== "image") {
-			/* i don't even want to think about whatever weird edge case would
-			   make this premature */
-			resolve(undefined);
-			return;
-		}
+		/* i don't even want to think about whatever weird edge case would
+		   make this premature */
+		if (type !== "image")
+			return resolve(undefined);
 
 		let bin = abtobs(this.response);
 		let data = bin;

commit 456bc08c0944660c9d9cf3af0432eed3a7ce873c

fix regression introduced in 87a4bd59

background.js
@@ -43,10 +43,6 @@ const arg = str => `"${str.split("").map(single =>
 	`\\u${`0000${single.charCodeAt(0).toString(16).toUpperCase()}`.slice(-4)}`
 ).join("")}"`;
 
-const literal_of = o => o === undefined
-	? "undefined"
-	: `JSON.parse(${arg(JSON.stringify(o))})`;
-
 const do_bookmark = async () => {
 	const prefs = await browser.storage.sync.get(defaults.sync),
 	      bookmark = await browser.storage.local.get(Object.keys(tab_reads)),
@@ -81,11 +77,14 @@ const do_bookmark = async () => {
 		}
 	}
 	try {
+		let iconarg = icon === undefined
+				? "undefined"
+				: `JSON.parse(${arg(JSON.stringify(favicon_fmt_out))})`;
 		bookmark_fmt_out = (await browser.tabs.executeScript({
 			code: `((rawtitle, title, url, icon) => {
 				${prefs.bookmark_fmt}
 			})(${arg(rawtitle)}, ${arg(title)}, ${arg(url)},
-			   ${literal_of(icon)})`
+			   ${iconarg})`
 		}))[0];
 	} catch (e) {
 		return creation_error(e, "bookmark_fmt");